7a61573f5feda6434773b40f5d7cb758c7a6c900
Tri Pham <phamthanhtri0712@gmail.com>
2025-01-20 10:01:29 +0700

    DAO ChiTietHoaDon, ChiTietCaLamViec

---
+++ src/main/java/dao/DAO_ChiTietCaLamViec.java
@@ -0,0 +1,10 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DAO_ChiTietCaLamViec {
+    private EntityManager em;
+
+}
---
+++ src/main/java/dao/DAO_ChiTietHoaDon.java
@@ -0,0 +1,15 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import lombok.AllArgsConstructor;
+import model.ChiTietHoaDon;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class DAO_ChiTietHoaDon {
+    private EntityManager em;
+
+
+}
