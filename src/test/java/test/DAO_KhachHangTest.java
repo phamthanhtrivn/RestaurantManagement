dc258fd6e308369dd84c121fc44bd5f137bcf7da
unknown <dangvanly270704@gmail.com>
2025-01-21 20:54:03 +0700

    test khach hang , LKH

--- src/test/java/test/DAO_KhachHangTest.java
+++ src/test/java/test/DAO_KhachHangTest.java
@@ -4,6 +4,7 @@ import dao.DAO_KhachHang;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
 import model.KhachHang;
+import model.LoaiKhachHang;
 import org.junit.jupiter.api.Test;
 import util.DataGenerator;
 
@@ -14,15 +15,37 @@ class DAO_KhachHangTest {
     @Test
     void addKhachHang() {
         DAO_KhachHang dao = new DAO_KhachHang(em);
-        KhachHang khachHang = DataGenerator.generateKhachHang();
+        var loaiKhachHang = em.createQuery("SELECT l FROM LoaiKhachHang l", LoaiKhachHang.class)
+                .setMaxResults(1) // Giới hạn kết quả để chỉ lấy một loại
+                .getSingleResult();
 
+        KhachHang khachHang = DataGenerator.generateKhachHang();
+        khachHang.setLoaiKH(loaiKhachHang);
+
+        boolean result = dao.addKhachHang(khachHang);
+        assertTrue(result);
     }
 
     @Test
     void updateKhachHang() {
         DAO_KhachHang dao = new DAO_KhachHang(em);
+
+        LoaiKhachHang loaiKhachHang = em.createQuery("SELECT l FROM LoaiKhachHang l", LoaiKhachHang.class)
+                .setMaxResults(1)
+                .getSingleResult();
+        if (loaiKhachHang == null) {
+            loaiKhachHang = new LoaiKhachHang();
+            loaiKhachHang.setTenLoaiKH("Default Type");
+            em.getTransaction().begin();
+            em.persist(loaiKhachHang);
+            em.getTransaction().commit();
+        }
+
         KhachHang khachHang = DataGenerator.generateKhachHang();
+        khachHang.setLoaiKH(loaiKhachHang);
+
         dao.addKhachHang(khachHang);
+
         khachHang.setTenKH("new customer name");
         assertTrue(dao.updateKhachHang(khachHang));
     }
@@ -30,10 +53,18 @@ class DAO_KhachHangTest {
     @Test
     void deleteKhachHang() {
         DAO_KhachHang dao = new DAO_KhachHang(em);
-        KhachHang khachHang = DataGenerator.generateKhachHang();
-        dao.addKhachHang(khachHang);
-        assertTrue(dao.deleteKhachHang(khachHang.getMaKH()));
+
+        var khachHang = em.createQuery("SELECT kh FROM KhachHang kh", KhachHang.class)
+                .setMaxResults(1)
+                .getSingleResult();
+
+        boolean result = dao.deleteKhachHang(khachHang.getMaKH());
+        assertTrue(result);
+
+        assertNull(dao.findByID(khachHang.getMaKH()));
     }
+
+
 
     @Test
     void findByID() {
